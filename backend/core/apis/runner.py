4365fb06fb35a0f7af7d2e0185840b29dae016d5
hhstore <hhstore@qq.com>
2017-05-30 21:19:55 +0800

    update - 对 HTTP 请求参数, 作合法性检查.

--- backend/core/apis/runner.py
+++ backend/core/apis/runner.py
@@ -2,16 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse, abort
 
 from backend.core.services.runner import CodeRunner
+from backend.core.settings import ContainerConfig
+
+parser = reqparse.RequestParser()
+
+# 参数校验:
+parser.add_argument("lang", type=str)
+parser.add_argument("code", type=str)
 
 
 class Runner(Resource):
     def post(self):
-        data = request.get_json()
-        lang = data.get("lang")
+        #data = request.get_json()
+        data = parser.parse_args()
+        lang = data.get("lang").lower()  # 语言类型不区分大小写
         code = data.get("code")
+
+        # 编程语言类型不支持:
+        if lang not in ContainerConfig.language_support:
+            abort(400)   # 参数不合法, 直接返回 [400 Bad Request]
 
         r = CodeRunner(code, lang)
         return r.run()
